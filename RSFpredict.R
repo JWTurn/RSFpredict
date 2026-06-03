856f06997d4d5538780e4683c567e64677a5fed6
Julie Turner <julwturner@gmail.com>
2026-06-03 11:54:59 -0700

    update to be dynamic

--- RSFpredict.R
+++ RSFpredict.R
@@ -21,6 +21,18 @@ defineModule(sim, list(
     #defineParameter("paramName", "paramClass", value, min, max, "parameter description"),
     defineParameter("numBins", "integer", 10, NA, NA,
                     "number of bins for RSF, default = 10"),
+    defineParameter("simulationProcess", "character", "static", NA, NA,
+                    paste0("Should the simulation use LandR (dynamic) or land cover map (static)?",
+                           "defaults to static")),
+    defineParameter("predictionInterval", "numeric", 5, NA, NA, "Time between predictions"),
+    defineParameter("predictStartYear", "numeric", 2025, NA, NA,
+                    paste0("The first year to start forecasted simulations if dynamic.",
+                           " This is because we start forecasting landcovers earlier.")),
+    defineParameter("predictLastYear", "logical", TRUE, NA, NA,
+                    paste0("If last year of simulation is not multiple of")),
+    defineParameter("ts_else", "integer", 100, NA, NA,
+                    paste0("This is the value to fill in NAs in time since disturbance layers",
+                           "This parameter would need to be updated if want a different default year.")),
     defineParameter(".plots", "character", "screen", NA, NA,
                     "Used by Plots function, which can be optionally used here"),
     defineParameter(".plotInitialTime", "numeric", start(sim), NA, NA,
@@ -42,18 +54,29 @@ defineModule(sim, list(
   ),
   inputObjects = bindrows(
     #expectsInput("objectName", "objectClass", "input object description", sourceURL, ...),
-    expectsInput(objectName = 'model', objectClass = 'glmmTMB',
+    expectsInput(objectName = 'model', objectClass = c('glmmTMB'),
                  desc = 'RSF model'),
     expectsInput(objectName = 'studyArea', objectClass = 'spatVector',
                  desc = 'unbuffered study area'),
-    expectsInput(objectName = 'landStack', objectClass = 'spatRaster',
-                 desc = 'stack of raster data to predict across')
+    expectsInput(objectName = 'modelLand', objectClass = 'spatRaster',
+                 desc = 'stack of raster data as start layers that data were extracted from'),
+    expectsInput("cohortData", "data.table",
+                 desc = paste("`data.table` with cohort-level information on age and biomass, by `pixelGroup` and ecolocation",
+                              "(i.e., `ecoregionGroup`). If supplied, it must have the following columns: `pixelGroup` (integer),",
+                              "`ecoregionGroup` (factor), `speciesCode` (factor), `B` (integer in $g/m^2$), `age` (integer in years)")),
+    expectsInput("pixelGroupMap", "SpatRaster",
+                 desc = paste("A raster layer with `pixelGroup` IDs per pixel. Pixels are grouped" ,
+                              "based on identical `ecoregionGroup`, `speciesCode`, `age` and `B` composition,",
+                              "even if the user supplies other initial groupings (e.g., via the `Biomass_borealDataPrep`",
+                              "module.")),
+    expectsInput(objectName = "timeSinceFire", objectClass = "SpatRaster",
+                 desc = "If dynamic, map of time since last burn used in model")
   ),
   outputObjects = bindrows(
     #createsOutput("objectName", "objectClass", "output object description", ...),
     createsOutput(objectName = "pred", objectClass = 'spatRaster', desc = 'raw predicted RSF'),
-    createsOutput(objectName = "binMap", objectClass = 'spatRaster', desc = 'binned predicted RSF'),
-    createsOutput(objectName = "p.rsf", objectClass = 'ggplot', desc = 'ggplot of RSF')
+    createsOutput(objectName = "binMap", objectClass = 'spatRaster', desc = 'binned predicted RSF')
+
   )
 ))
 
@@ -64,11 +87,38 @@ doEvent.RSFpredict = function(sim, eventTime, eventType) {
       ### check for more detailed object dependencies:
       ### (use `checkObject` or similar)
 
+
+      # Baseline always runs
+      sim <- scheduleEvent(sim, time(sim), "RSFpredict", "buildBaselineRSFmap")
+
+      if (Par$simulationProcess == "dynamic") {
+
+        # always run simLayers
+        sim <- scheduleEvent(sim, P(sim)$predictStartYear, "RSFpredict", "simLayers")
+        sim <- scheduleEvent(sim, P(sim)$predictStartYear, "RSFpredict", "simRSFmap")
+
+      }
+
+      if (Par$predictLastYear) {
+        sim <- scheduleEvent(sim, end(sim), "RSFpredict", "simLayers")
+      }
+
+
+
+      # schedule future event(s)
+      # sim <- scheduleEvent(sim, P(sim)$.plotInitialTime, "RSFpredict", "plot")
+      # sim <- scheduleEvent(sim, P(sim)$.saveInitialTime, "RSFpredict", "save")
+    },
+
+    buildBaselineRSFmap = {
+
+      message("Building baseline RSF map")
+
       sim$pred <- terra::predict(sim$landStack, sim$model, type = "response", re.form = NA) |>
         Cache()
-        # predict(sim$model,
-        #               newdata = sim$landStack,#[!is.na(Covar.brick.values$DEM),],
-        #               allow.new.levels=TRUE)
+      # predict(sim$model,
+      #               newdata = sim$landStack,#[!is.na(Covar.brick.values$DEM),],
+      #               allow.new.levels=TRUE)
       pred.mask <- terra::mask(sim$pred, sim$studyArea)
       # TODO get rid of weird extremes
       # quantPred <- global(pred.mask, quantile, probs = c(0.9), na.rm = T)
@@ -77,129 +127,137 @@ doEvent.RSFpredict = function(sim, eventTime, eventType) {
       v.breaks <- t(breaks)
       sim$binMap <- terra::classify(pred.mask, v.breaks, include.lowest=TRUE, brackets=TRUE)
 
-      sim$p.rsf <- ggplot() +
-        geom_spatraster(data = as.numeric(sim$binMap, show.legend = T)) +
-        scale_fill_viridis(option = 'viridis', na.value = NA, name = 'Intensity of Selection') +
-        theme_minimal() +
-        labs(x = NULL, y = NULL)
-      sim$p.rsf
+      # p.rsf <- ggplot() +
+      #   geom_spatraster(data = as.numeric(sim$binMap, show.legend = T)) +
+      #   scale_fill_viridis(option = 'viridis', na.value = NA, name = 'Intensity of Selection') +
+      #   theme_minimal() +
+      #   labs(x = NULL, y = NULL)
+      # p.rsf
+      #
+      # outPath <- outputPath(sim)
+      # ggsave(plot = p.rsf, filename = file.path(outPath, 'map.png'))
+      #
 
-      outPath <- outputPath(sim)
-      ggsave(plot = sim$p.rsf, filename = file.path(outPath, 'map.png'))
-
-
-      # schedule future event(s)
-      # sim <- scheduleEvent(sim, P(sim)$.plotInitialTime, "RSFpredict", "plot")
-      # sim <- scheduleEvent(sim, P(sim)$.saveInitialTime, "RSFpredict", "save")
     },
-    plot = {
-      # ! ----- EDIT BELOW ----- ! #
-      # do stuff for this event
 
-      plotFun(sim) # example of a plotting function
-      # schedule future event(s)
+    simLayers = {
 
-      # e.g.,
-      #sim <- scheduleEvent(sim, time(sim) + P(sim)$.plotInterval, "RSFpredict", "plot")
+      thisYear <- as.integer(time(sim))
+      key <- paste0("year", thisYear)
+      message(paste0("Simulating landscape for ", thisYear)
 
-      # ! ----- STOP EDITING ----- ! #
+      template <- sim$rasterToMatch
+
+      templateCoarse <- terra::aggregate(template, fact = 2)
+
+
+      # Dynamic forest layers from LandR
+      reclassForest <- reclassifyCohortData(
+        cohortData = sim$cohortData,
+        sppEquivCol = "LandR",
+        pixelGroupMap = sim$pixelGroupMap,
+        mixedForestCutoffs = c(0.33, 0.66)
+      )
+
+      ft <- reclassForest$`forest type`
+
+
+        # Needleleaf
+        needle_mask <- terra::classify(
+          ft, rcl = matrix(c(210, 1), ncol = 2, byrow = TRUE), others = 0
+        )
+        prop_needleleaf <- terra::resample(needle_mask, template, method = "average")
+        names(prop_needleleaf) <- "prop_needleleaf"
+
+        # Mixedforest
+        mixed_mask <- terra::classify(
+          ft, rcl = matrix(c(220, 1, 230, 1), ncol = 2, byrow = TRUE),
+          others = 0
+        )
+        prop_mixedforest <- terra::resample(mixed_mask, template, method = "average")
+        names(prop_mixedforest) <- "prop_mixedforest"
+
+        # scale up to RSF res
+        forests <- reproducible::postProcess(c(prop_needleleaf, prop_mixedforest), templateCoarse, method = 'average')
+
+
+      message("Complated reclassifying forest layers")
+
+
+      # Dynamic timeSinceFire from scfmSpread
+      tsf <- reproducible::postProcess(sim$timeSinceFire, to = templateCoarse)
+      tsf[is.na(tsf)] <- P(sim)$ts_else + (thisYear - 2020)) #TODO this is hardcoded for now, now grab start time?
+
+      message("Updated timeSinceFire using scfm for RSF")
+
+      names(tsf) <- "timeSinceFire"
+
+      tsRasts <- tsf
+
+
+
+      # Bundle bundle updates
+      sim$simLand[[key]] <- c(forests, tsRasts)
+      # simLand needs to be exported to workflowOutputs for normalization model
+
+      # sim$simEnv[[key]] <- list2env(
+      #   setNames(lapply(names(simLand), \(n) simLand[[n]]), names(simLand)),
+      #   parent = baseenv()
+      # )
+      message("needleleaf mean: ", terra::global(prop_needleleaf, "mean", na.rm=TRUE)[1,1])
+      message("mixed mean: ", terra::global(prop_mixedforest, "mean", na.rm=TRUE)[1,1])
+      message("tsf mean: ", terra::global(tsf, "mean", na.rm=TRUE)[1,1])
+
+
+
+      # save layers
+      outDir <- reproducible::checkPath(file.path(outputPath(sim), paste0(Par$.studyAreaName, '_', 'sims'), create = T)
+
+
+        terra::writeRaster(sim$simLand[[key]], file.path(outDir, paste0("mapLayers_", Par$.studyAreaName, "_", key, ".tif")), overwrite = TRUE)
+
+
+      sim <- scheduleEvent(sim, time(sim) + P(sim)$predictionInterval, "RSFpredict", "simLayers")
     },
-    save = {
-      # ! ----- EDIT BELOW ----- ! #
-      # do stuff for this event
 
-      # e.g., call your custom functions/methods here
-      # you can define your own methods below this `doEvent` function
+    simRSFmap = {
+      thisYear <- as.integer(time(sim))
+      key <- paste0("year", thisYear)
+      message(paste0("Calculating simulated RSF map for: ", thisYear))
+      if (is.null(sim$simEnv[[key]]))
+        stop("Missing sim$simEnv[['", key, "']]. Did simLayers run first?")
 
-      # schedule future event(s)
 
-      # e.g.,
-      # sim <- scheduleEvent(sim, time(sim) + P(sim)$.saveInterval, "RSFpredict", "save")
+      outDir <- reproducible::checkPath(file.path(outputPath(sim), paste0(Par$.studyAreaName), '_', 'sims'), create = T)
 
-      # ! ----- STOP EDITING ----- ! #
+      sim$simPred[[key]] <- terra::predict(sim$landStack, sim$model, type = "response", re.form = NA) |>
+        Cache()
+
+      pred.mask <- terra::mask(sim$simPred, sim$studyArea)
+      # TODO get rid of weird extremes
+      # quantPred <- global(pred.mask, quantile, probs = c(0.9), na.rm = T)
+      # predClmp <- terra::clamp(pred.mask, upper = quantPred[[1]])
+      breaks <- terra::global(pred.mask, quantile, na.rm = T, probs = seq(0,1,1/Par$numBins))
+      v.breaks <- t(breaks)
+      sim$simBinMap[[key]] <- terra::classify(pred.mask, v.breaks, include.lowest=TRUE, brackets=TRUE)
+
+
+        terra::writeRaster(pred.mask, file.path(outDir, paste0("rsfMap", Par$.studyAreaName, '_', key, '.tif')), overwrite = TRUE)
+        terra::writeRaster(sim$simBinMap[[key]], file.path(outDir, paste0("rsfBinMap_", Par$.studyAreaName, '_', key, '.tif')), overwrite = TRUE)
+
+
+
+
+      message(paste0("Finished creating RSF for: ", thisYear))
+      message(paste0("Outputs saved to: ", outDir))
+      sim <- scheduleEvent(sim, time(sim) + P(sim)$predictionInterval, "RSFpredict", "simRSFmap")
     },
-    event1 = {
-      # ! ----- EDIT BELOW ----- ! #
-      # do stuff for this event
 
-      # e.g., call your custom functions/methods here
-      # you can define your own methods below this `doEvent` function
-
-      # schedule future event(s)
-
-      # e.g.,
-      # sim <- scheduleEvent(sim, time(sim) + increment, "RSFpredict", "templateEvent")
-
-      # ! ----- STOP EDITING ----- ! #
-    },
-    event2 = {
-      # ! ----- EDIT BELOW ----- ! #
-      # do stuff for this event
-
-      # e.g., call your custom functions/methods here
-      # you can define your own methods below this `doEvent` function
-
-      # schedule future event(s)
-
-      # e.g.,
-      # sim <- scheduleEvent(sim, time(sim) + increment, "RSFpredict", "templateEvent")
-
-      # ! ----- STOP EDITING ----- ! #
-    },
     warning(noEventWarning(sim))
   )
   return(invisible(sim))
-}
+  #return(sim)
 
-### template initialization
-Init <- function(sim) {
-  # # ! ----- EDIT BELOW ----- ! #
-
-  # ! ----- STOP EDITING ----- ! #
-
-  return(invisible(sim))
-}
-### template for save events
-Save <- function(sim) {
-  # ! ----- EDIT BELOW ----- ! #
-  # do stuff for this event
-  sim <- saveFiles(sim)
-
-  # ! ----- STOP EDITING ----- ! #
-  return(invisible(sim))
-}
-
-### template for plot events
-plotFun <- function(sim) {
-  # ! ----- EDIT BELOW ----- ! #
-  # do stuff for this event
-  sampleData <- data.frame("TheSample" = sample(1:10, replace = TRUE))
-  Plots(sampleData, fn = ggplotFn) # needs ggplot2
-
-  # ! ----- STOP EDITING ----- ! #
-  return(invisible(sim))
-}
-
-### template for your event1
-Event1 <- function(sim) {
-  # ! ----- EDIT BELOW ----- ! #
-  # THE NEXT TWO LINES ARE FOR DUMMY UNIT TESTS; CHANGE OR DELETE THEM.
-  # sim$event1Test1 <- " this is test for event 1. " # for dummy unit test
-  # sim$event1Test2 <- 999 # for dummy unit test
-
-  # ! ----- STOP EDITING ----- ! #
-  return(invisible(sim))
-}
-
-### template for your event2
-Event2 <- function(sim) {
-  # ! ----- EDIT BELOW ----- ! #
-  # THE NEXT TWO LINES ARE FOR DUMMY UNIT TESTS; CHANGE OR DELETE THEM.
-  # sim$event2Test1 <- " this is test for event 2. " # for dummy unit test
-  # sim$event2Test2 <- 777  # for dummy unit test
-
-  # ! ----- STOP EDITING ----- ! #
-  return(invisible(sim))
 }
 
 .inputObjects <- function(sim) {
@@ -222,6 +280,11 @@ Event2 <- function(sim) {
   message(currentModule(sim), ": using dataPath '", dPath, "'.")
 
   # ! ----- EDIT BELOW ----- ! #
+  if (!suppliedElsewhere("model", sim)) {
+
+      stop("RSF model not provided")
+
+  }
 
   # ! ----- STOP EDITING ----- ! #
   return(invisible(sim))
